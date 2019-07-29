fcfef11e219b6a9df55a95195ff216d304fa3e07
ZhangXuesong <zhangxs@udesk.cn>
2019-07-29 11:53:09 +0800

    Fix bug

--- kong/plugins/hello-udesk/schema.lua
+++ kong/plugins/hello-udesk/schema.lua
@@ -6,11 +6,10 @@ return {
     { run_on = typedefs.run_on_first },
     { protocols = typedefs.protocols_http },
     { consumers = typedefs.no_consumer },
-    {
-      config = {
+    { config = {
         type = "record",
         fields = {
-          can_say_hello = { type = "boolean", default = true }
+          { can_say_hello = { type = "boolean", default = true } }
         }
       }
     }
