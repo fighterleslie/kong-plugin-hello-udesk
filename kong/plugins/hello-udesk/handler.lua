bd5be28ebf9cf5f645e21c1ba518e85caf7372aa
ZhangXuesong <zhangxs@udesk.cn>
2019-07-29 11:47:54 +0800

    Fix bug

--- kong/plugins/hello-udesk/handler.lua
+++ kong/plugins/hello-udesk/handler.lua
@@ -3,7 +3,7 @@ local HelloUdeskHandler = {}
 HelloUdeskHandler.PRIORITY = 8
 HelloUdeskHandler.VERSION = '0.0.1'
 
-function HelloUdeskHandler::header_filter(conf)
+function HelloUdeskHandler:header_filter(conf)
   kong.response.add_header("X-Greet", "Hello Udesk")
 end
 
